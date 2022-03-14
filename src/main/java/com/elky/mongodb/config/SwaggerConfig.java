e5cc0121f137eeae0ee53c0f9dfd66f51a88e2f1
Elky <elky@krafton.com>
2022-03-14 15:25:55 +0900

    Swagger 설정 기본 패키지 수정

--- src/main/java/com/elky/mongodb/config/SwaggerConfig.java
+++ src/main/java/com/elky/mongodb/config/SwaggerConfig.java
@@ -14,7 +14,7 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.elky.jpa")).paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.elky.mongodb")).paths(PathSelectors.any())
                 .build();
 
     }
